418a6cb4cab607c4b84c963faf23b84512bbc774
Aurora Scharff <aurora.sofie@gmail.com>
2026-07-30 03:42:10 -0400

    update

--- features/message/components/channel-detail-panel.tsx
+++ features/message/components/channel-detail-panel.tsx
@@ -2,22 +2,32 @@
 
 import { type ReactNode } from 'react'
 import { ResizablePanel } from '@/components/ui/resizable-panel'
-import { useThread } from './use-thread'
 import { ThreadPanel } from './thread-panel'
+import { useThread } from './use-thread'
 
 export function ChannelDetailPanel({ details }: { details: ReactNode }) {
   const { activeThread } = useThread()
 
   return (
-    <ResizablePanel>
+    <>
+      <ResizablePanel>
+        {activeThread ? (
+          <ThreadPanel
+            channelId={activeThread.channelId}
+            messageId={activeThread.messageId}
+          />
+        ) : (
+          details
+        )}
+      </ResizablePanel>
       {activeThread ? (
-        <ThreadPanel
-          channelId={activeThread.channelId}
-          messageId={activeThread.messageId}
-        />
-      ) : (
-        details
-      )}
-    </ResizablePanel>
+        <div className="bg-surface dark:bg-surface-dark fixed inset-0 z-50 flex flex-col lg:hidden">
+          <ThreadPanel
+            channelId={activeThread.channelId}
+            messageId={activeThread.messageId}
+          />
+        </div>
+      ) : null}
+    </>
   )
 }
