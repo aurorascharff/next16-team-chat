a4ee90ae63ba09a0feaf222f5e39afed409f24de
Aurora Scharff <aurora.sofie@gmail.com>
2026-08-01 15:28:26 +0200

    update

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function delay(ms: number, enabled = true) {
+export function delay(ms: number, enabled: boolean) {
   return enabled
     ? new Promise((resolve) => setTimeout(resolve, ms))
     : Promise.resolve()
